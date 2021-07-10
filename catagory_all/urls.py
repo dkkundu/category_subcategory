4e1ce74611b5972614f467a7c4734c475ec4ed5e
Dipto K Kundu <dkkundu00@gmail.com>
2021-07-10 15:31:59 +0600

    add allfile

---
+++ catagory_all/urls.py
@@ -0,0 +1,12 @@
+
+from django.conf import settings
+
+from django.urls import include, path
+
+
+urlpatterns = [
+
+    # Core urls ---------------------------------------------------------------
+    # path('core/', include('Core.urls')),
+
+]
